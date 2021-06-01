64e52fa2c0a19167d7786620ef0c56a62a02d126
liran meir <liran.m@lawgeex.com>
2021-06-02 00:42:47 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-dca9a8acb8814b5dec7e.js"
+    "url": "webpack-runtime-fd4c1dbb906e3ba776f3.js"
   },
   {
     "url": "styles.008998c4d0294c85c988.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2647f22a6502f6febdfaaf3f8b2db68e"
+    "revision": "701a090b8cf2183e55b009cdc548cb21"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7188043127c4e8798842.js"
@@ -57,7 +57,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "78daa36eec0de24363f19948cfd2f86c"
+    "revision": "fcbd17505a25166715fd514946b91b73"
   },
   {
     "url": "manifest.webmanifest",
